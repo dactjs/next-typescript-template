53221ef8d340d27b2519405f2ef79c1cbccd2752
Diego Cruz <dactjs@gmail.com>
2022-04-09 09:55:51 -0400

    refactor: layout basics

--- components/layout/Dashboard/index.tsx
+++ components/layout/Dashboard/index.tsx
@@ -1,19 +1,15 @@
 import styles from './Dashboard.module.css';
 
-export const Dashboard: React.FC = ({ children }) => (
+export const Dashboard: React.FC<React.PropsWithChildren<unknown>> = ({
+  children,
+}) => (
   <div className={styles.layout}>
     <header className={styles.header}>
-      <strong>Logo</strong>
+      <strong style={{ margin: 'auto' }}>Header</strong>
     </header>
 
     <aside className={styles.sidebar}>
-      <nav>
-        <ul>
-          <li>Overview</li>
-          <li>Users</li>
-          <li>Settings</li>
-        </ul>
-      </nav>
+      <strong style={{ margin: 'auto' }}>Sidebar</strong>
     </aside>
 
     <div className={styles.content}>{children}</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { EnhancedNextPage } from '@/types';
 
 const Home: EnhancedNextPage = () => (
   <Page title="Next + TypeScript" description="Another Next App template">
-    <main>
-      <h1>Building Something Great!</h1>
+    <main style={{ margin: 'auto' }}>
+      <h1>Content</h1>
     </main>
   </Page>
 );
